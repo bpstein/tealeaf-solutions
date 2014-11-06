b937af70439991fb49fe59047495892ff6c44e07
Ben Stein <bumstain@gmail.com>
2014-11-06 23:51:48 +0000

    Completed the basics

--- the_basics.rb
+++ the_basics.rb
@@ -1,6 +1,46 @@
+# 1. String concatenation
 puts "Ben " + "Stein"
+puts " "
 
+# 2. Basic operations
 puts 5422 / 1000
 puts 5422 % 1000 / 100
 puts 5422 % 1000 % 100 / 10
 puts 5422 % 1000 % 100 % 10
+puts " "
+
+# 3. Storing and retrieving data from a hash
+movies = {jaws: 1971, 
+					empire: 1981, 
+					memento: 2004, 
+					avatar: 2010}
+
+puts movies[:jaws]
+puts movies[:empire]
+puts movies[:memento]
+puts movies[:avatar]
+puts " "
+
+# 4. Storing and retrieving data from an array
+dates = [1971, 1981, 2004, 2010]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts " "
+
+# 5. Calculation of a factorial (5!, 6!, 7!, 8!)
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts " "
+
+# 6. Calculation of squares of three float numbers
+puts 524.88 * 524.88
+puts 5662233.1 * 5662233.1
+puts 9.1 * 9.1
+puts " "
+
+# 7. A parenthesis was opened without being closed somewhere in the code block.
