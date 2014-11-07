249d4b7de5f51f28b5c6a817496705878d4eeec5
Ben Stein <bumstain@gmail.com>
2014-11-07 01:07:36 +0000

    Completed methods

---
+++ method.rb
@@ -0,0 +1,23 @@
+# 1. Greeting method
+def greeting(name) 
+	puts "Welcome " + name + "!"
+end
+
+puts greeting("Ben")
+puts " "
+
+
+# 2. Multiply method
+def multiply(x, y)
+	x * y
+end
+
+puts multiply(4, 4)
+puts " "
+
+# 5. Print to screen method
+def scream(words)
+  words = words + "!!!!"
+end
+
+puts scream("Yippeee") 
