bf50934c197e84338992f725f463f1fb4a0ff4e0
Ben Stein <bumstain@gmail.com>
2014-11-07 00:05:06 +0000

    Completed variables

---
+++ name.rb
@@ -0,0 +1,16 @@
+# 1. Take name and print greeting message
+puts "What is your name?"
+name = gets.chomp
+puts "Hello " + name
+
+# 3. Print name 10 times
+10.times do 
+	puts name
+end
+
+# 4. Take and print first name and last name
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts first_name + " " + last_name
