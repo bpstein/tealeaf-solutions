e549c6a0ac8d62bca0cd0110deb9e2e731b103e7
Ben Stein <bumstain@gmail.com>
2014-11-10 23:56:38 +0000

    Completed object model

--- name.rb
+++ name.rb
@@ -14,3 +14,6 @@ first_name = gets.chomp
 puts "What is your last name?"
 last_name = gets.chomp
 puts first_name + " " + last_name
+
+
+
---
+++ object_model.rb
@@ -0,0 +1,15 @@
+# 1. Give example of creation of an object
+class ExampleClass
+end
+
+new_class = ExampleClass.new
+
+# 2. Create a module
+module Practice
+end
+
+class ExampleClass
+  include Practice
+end
+
+new_class = ExampleClass.new
