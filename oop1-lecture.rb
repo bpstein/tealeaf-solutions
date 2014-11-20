373ade00d1d46b31e6232e4e53fb445bf2decc62
Ben Stein <bumstain@gmail.com>
2014-11-20 00:04:30 +0000

    Completed OOP lecture 1

--- oop1-lecture.rb
+++ oop1-lecture.rb
@@ -1,28 +1,40 @@
 # OOP in Ruby
 
 # 1. classes and objects
+# 2. methods contain behaviors
+# 3. instance variables contain states
+# 4. objects are instantiated from classes, and contain states and behaviors
+# 5. class variables and methods
+# 6. compare with procedural
 
 class Dog
+	attr_accessor :name, :height, :weight
+
+	@@count = 0 
+
+	def self.total_dogs
+		"Total number of dogs: #{@@count}"
+	end
+
 	def initialize(n, h, w)
 		@name = n 
 		@height = h
 		@weight = w 
+		@@count += 1
 	end
 
 	def speak
 		@name + " barks!"
 	end
 
-	def name
-		@name
-	end
-
-	def name=(new_name)
-		@name = new_name
-	end
-
 	def info
-		"#{@name} is #{@height} tall and weighs #{@weight} pounds"
+		"#{name} is #{height} tall and weighs #{weight} pounds"
+	end
+
+	def update_info(n, h, w)
+		self.name = n
+		self.height = h
+		self.weight = w
 	end
 end
 
@@ -34,7 +46,8 @@ fido = Dog.new('fido', 1, 35)
 puts teddy.speak
 puts fido.speak
 
-teddy.name = "Roosevelt"
-
 puts teddy.info
-puts fido.info
+teddy.update_info('Roosevelt', 5, 125)
+puts teddy.info
+
+puts Dog.total_dogs
